fb1adc01e9cb4a09efd6c7cc5f69517b37d38b41
garsin <garsinap@gmail.com>
2018-02-28 16:31:36 +0530

    user_2 change_1

--- Desktop/user1/cs251/hello.c
+++ Desktop/user1/cs251/hello.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
+void microkernel_sendmsg(char *);
+
 int main(){
 	printf("HelloWorld\n");
+	microkernel_sendmsg("is more portable");
 	return 0;
+}
+
+void microkernel_sendmsg(char *a){ printf("microkernel: %s\n", a);
 }
